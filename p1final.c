76e29a1e5a996733073a08a6af9d1732efa7f87a
Prajwal D <96919382+Prajwald5@users.noreply.github.com>
2022-04-01 04:16:23 +0000

    p1

--- p1final.c
+++ p1final.c
@@ -1,1 +1,39 @@
-#include <stdio.h>
+#include<stdio.h>
+#include<math.h>
+float input_radius(){
+    float n;
+    printf("Enter the radius of stomach of the camel\n");
+    scanf("%f",&n);
+    return n;
+}
+float input_height(){
+    float n;
+    printf("Enter the height of the camel\n");
+    scanf("%f",&n);
+    return n;
+}
+float input_length(){
+    float n;
+    printf("Enter the length of the camel\n");
+    scanf("%f",&n);
+    return n;
+}
+float find_weight(float radius,float height,float length){
+    float weight;
+    weight=3.14 * (radius*radius*radius) * sqrt(height * length);
+    return weight;
+}
+void output(float radius, float height, float length, float weight){
+    printf("The weight of the camel is %.2f",weight);
+}
+int main(){
+    float radius,height,length,weight;
+    radius=input_radius();
+    height=input_height();
+    length=input_length();
+    weight=find_weight(radius,height,length);
+    output(radius,height,length,weight);
+    return 0;
+
+
+}
